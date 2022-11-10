b622bf26239b14d4a8484ed03632d2197deba000
Hậu Phan Duy <phanduyhau2389@gmail.com>
2022-11-10 20:40:05 +0700

    update pt2

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,7 +12,7 @@ import Typiccal from "./Typical/Typiccal";
 function Home() {
   const [data, setData] = useState([]);
   useEffect(() => {
-    fetch("hhttps://json-server-augustus-game.herokuapp.com/products")
+    fetch("https://json-server-augustus-game.herokuapp.com/products")
       .then((response) => response.json())
       .then((data) => setData(data));
   }, []);
